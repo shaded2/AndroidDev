5afd0b5cd7d9a1025cf56de405bea102de899e25
shaded2 <shaded2@gmail.com>
2012-03-11 17:47:30 -0400

    chapter 7

--- TriviaQuiz/gen/com/androidbook/triviaquiz/R.java
+++ TriviaQuiz/gen/com/androidbook/triviaquiz/R.java
@@ -8,10 +8,41 @@
 package com.androidbook.triviaquiz;
 
 public final class R {
+    public static final class anim {
+        public static final int custom_anim=0x7f040000;
+        public static final int fade_in=0x7f040001;
+        public static final int fade_in2=0x7f040002;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int logo_color=0x7f050000;
+        public static final int version_bkgrd=0x7f050002;
+        public static final int version_color=0x7f050001;
+    }
+    public static final class dimen {
+        public static final int logo_size=0x7f060000;
+        public static final int version_size=0x7f060001;
+        public static final int version_spacing=0x7f060002;
     }
     public static final class drawable {
         public static final int quizicon=0x7f020000;
+        public static final int splash1=0x7f020001;
+        public static final int splash2=0x7f020002;
+        public static final int splash3=0x7f020003;
+        public static final int splash4=0x7f020004;
+    }
+    public static final class id {
+        public static final int ImageView2_Left=0x7f080003;
+        public static final int ImageView2_Right=0x7f080004;
+        public static final int ImageView3_Left=0x7f080006;
+        public static final int ImageView3_Right=0x7f080007;
+        public static final int TableLayout01=0x7f080001;
+        public static final int TableRow01=0x7f080002;
+        public static final int TableRow02=0x7f080005;
+        public static final int TextViewBottomTitle=0x7f080008;
+        public static final int TextViewBottomVersion=0x7f080009;
+        public static final int TextViewTopTitle=0x7f080000;
     }
     public static final class layout {
         public static final int game=0x7f030000;
@@ -22,12 +53,15 @@ public final class R {
         public static final int splash=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int game=0x7f040005;
-        public static final int help=0x7f040001;
-        public static final int menu=0x7f040002;
-        public static final int scores=0x7f040006;
-        public static final int settings=0x7f040004;
-        public static final int splash=0x7f040003;
+        public static final int app_logo_bottom=0x7f070008;
+        public static final int app_logo_top=0x7f070007;
+        public static final int app_name=0x7f070000;
+        public static final int app_version_info=0x7f070009;
+        public static final int game=0x7f070005;
+        public static final int help=0x7f070001;
+        public static final int menu=0x7f070002;
+        public static final int scores=0x7f070006;
+        public static final int settings=0x7f070004;
+        public static final int splash=0x7f070003;
     }
 }
